9c0a5d00c80a004e3506cb500010071449f41423
davidpsousa <62971705+prodigyq@users.noreply.github.com>
2023-03-18 16:17:45 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -3,13 +3,13 @@
 
 int main()
 {
-    char nome[100] = {'Joel Miranda', 'Kelton Cantao', 'David Sousa', 'Carolyn'};
-    char matricula[100] = {'202206840054', '202007040033','202207040045','202007040033'};
+    char nome[30] = {'Joel Miranda', 'Kelton Cantao', 'David Sousa', 'Carolyn'};
+    char matricula[30] = {'202206840054', '202007040033','202207040045','202007040033'};
     printf("Nome dos integrantes: ");
-        for(int i = 0; i < 2 ; i++)
-            printf("Nome: %s",nome[i]);
+       for(int i = 0; i < 4 ; i++)
+            printf("Nome: %s\n",nome[i]);
     printf("matricula dos integrantes: ");
-        for(int i = 0; i < 2 ; i++)
-            printf("matricula: %s",matricula[i]);
+        for(int i = 0; i < 4 ; i++)
+            printf("matricula: %s\n",matricula[i]);
     return 0;
 }
