1cf97beeaf314749ab2e426718813c53e3d7d770
Joel Miranda <127310715+JoeMiran@users.noreply.github.com>
2023-03-17 22:02:08 -0300

    Create atividade_1.c

---
+++ atividade_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+   
+   char nome = "Joel Tavares Miranda";
+   int matricula = 202206840054;
+   
+   printf("Nome: %s\nMatricula: %d\n", nome, matricula);
+   
+   return 0;
+}
