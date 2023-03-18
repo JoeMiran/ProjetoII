52cee44557bf31127441f5da2badadc7be5c3bd3
Joel Miranda <127310715+JoeMiran@users.noreply.github.com>
2023-03-18 17:32:06 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -5,11 +5,12 @@ int main()
 {
     char nome[30] = {'Joel Miranda', 'Kelton Cantao', 'David Sousa', 'Carolyn'};
     char matricula[30] = {'202206840054', '202007040033','202207040045','202007040033'};
+    int i;
     printf("Nome dos integrantes: ");
-       for(int i = 0; i < 4 ; i++)
+       for(i = 0; i < 4 ; i++)
             printf("Nome: %s\n",nome[i]);
     printf("matricula dos integrantes: ");
-        for(int i = 0; i < 4 ; i++)
+        for(i = 0; i < 4 ; i++)
             printf("matricula: %s\n",matricula[i]);
     return 0;
 }
