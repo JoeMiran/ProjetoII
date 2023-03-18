0c9d5e4078b631deccdd74b45dda595a1656c6a1
KeltonCantao <128101430+KeltonCantao@users.noreply.github.com>
2023-03-18 19:24:28 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -3,14 +3,8 @@
 
 int main()
 {
-    char nome[30] = {'Joel Miranda', 'Kelton Cantao', 'David Sousa', 'Carolyn'};
-    char matricula[30] = {'202206840054', '202007040033','202207040045','202007040033'};
-    int i;
-    printf("Nome dos integrantes: ");
-       for(i = 0; i < 4 ; i++)
-            printf("Nome: %s\n",nome[i]);
-    printf("matricula dos integrantes: ");
-        for(i = 0; i < 4 ; i++)
-            printf("matricula: %s\n",matricula[i]);
+    char A1[30] = "Joel Miranda", A2[30] = "Kelton Cantao", A3[30] = "David Sousa", A4[30] = "Carolyn Aviz";
+    long B1 = 202206840054, B2 = 202007040033, B3 = 202207040045, B4 = 123;
+    printf("%s\t%ld \n%s\t%ld \n%s\t%ld \n%s\t%ld", A1, B1, A2, B2, A3, B3, A4, B4);
     return 0;
 }
