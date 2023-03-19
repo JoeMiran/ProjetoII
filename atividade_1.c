a0ccb4cfde065468a3a19e437a9439533d63a25c
Joel Miranda <127310715+JoeMiran@users.noreply.github.com>
2023-03-18 23:50:34 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    char A1[30] = "Joel Miranda", A2[30] = "Kelton Cantao", A3[30] = "David Sousa", A4[30] = "Carolyn Aviz";
-    long B1 = 202206840054, B2 = 202007040033, B3 = 202207040045, B4 = 123;
-    printf("%s\t%ld \n%s\t%ld \n%s\t%ld \n%s\t%ld", A1, B1, A2, B2, A3, B3, A4, B4);
+int main() {
+    int i;  
+    char nomes[4][30] = {
+      "Joel Tavares", 
+      "David Sousa", 
+      "Kelton Cantao",
+      "Carolyn"
+    };
+    char matriculas[4][30] = {
+      "202206840054", 
+      "202207040045", 
+      "202007040033", 
+      "202007040000"
+    };  
+    
+    printf("Nomes completos e matriculas dos alunos:\n");
+    for (i = 0; i < 4; i++) {
+        printf("%s - %s\n", nomes[i], matriculas[i]);
+    }
+
     return 0;
 }
