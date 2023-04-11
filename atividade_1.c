c8839d2e900c519698cccafd681944b1759d09f8
KeltonCantao <128101430+KeltonCantao@users.noreply.github.com>
2023-04-11 19:28:51 -0300

    Update atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -7,18 +7,8 @@ ajustável e dinâmico.*/
 int main() {
     // Inicio da apresentação da equipe.
     int i;  
-    char nomes[4][30] = {
-      "Joel Tavares", 
-      "David Sousa", 
-      "Kelton Cantao",
-      "Carolyn Aviz"
-    };
-    char matriculas[4][30] = {
-      "202206840054", 
-      "202207040045", 
-      "202007040033", 
-      "202106840046",
-    };  
+    char nomes[4][30] = {"Joel Tavares", "David Sousa", "Kelton Cantao", "Carolyn Aviz"};
+    char matriculas[4][30] = {"202206840054", "202207040045", "202007040033", "202106840046"};
     
     printf("Nomes completos e matriculas dos alunos:\n");
     for (i = 0; i < 4; i++) {
@@ -28,9 +18,9 @@ int main() {
     
     int A[m][n], B[o][p], C[m][p];	// Perceba que, se o número de colunas na matriz A (n) é igual ao numero de linhas na matriz B (p), a matriz resultante C será de dimensão m,p.
     printf("\nDimensões da Matriz A: ");
-	scanf("%d%d", &m, &n);
-	printf("\nDimensões da Matriz B; ");
-	scanf("%d%d", &o, &p);
+    scanf("%d%d", &m, &n);
+    printf("\nDimensões da Matriz B: ");
+    scanf("%d%d", &o, &p);
     
     return 0;
 }
