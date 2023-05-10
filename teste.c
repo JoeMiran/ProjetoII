003c8cca956f4be170de82ddf71f1f1b9df4ca23
JoeMiran <joemiran06@gmail.com>
2023-05-10 13:43:00 -0300

    Agora foi  Your branch is up to date with 'origin/main'.  Changes to be committed:

--- teste.c
+++ teste.c
@@ -37,10 +37,6 @@ complexMatrix allocateComplexMaatrix(int linhas, int colunas) {
     return matrix;
 }
 
-void printComplex(complex num) {
-    printf("%.2f + %.2fi\n", num.Re, num.Im);
-}
-
 complexMatrix matrixTransposta(complexMatrix matrix) {
 
     complexMatrix transposta;
@@ -73,11 +69,67 @@ complexMatrix matrixTransposta(complexMatrix matrix) {
     return transposta;
 }
 
+complexMatrix matrixConjugada(complexMatrix matrix) {
+
+    complexMatrix conjugada; //Declarando a matriz conjugada
+    
+    //A matriz conjugada vai receber as mesmas linhas e colunas da matriz original
+    conjugada.linhas = matrix.linhas; 
+    conjugada.colunas = matrix.colunas;
+
+    //Alocando memória para as linhas
+    conjugada.mtx = (complex**)malloc(conjugada.linhas * sizeof(complex*));
+    //Verificando se não houve nenhum erro na alocação das linhas
+    if(conjugada.mtx == NULL) {
+        printf("Erro na alocação da memória");
+        exit(1);
+    }
+
+    //Alocando memória para as colunas de cada linha
+    for (int i = 0; i < conjugada.linhas; i++) {
+        conjugada.mtx[i] = (complex*)malloc(conjugada.colunas * sizeof(complex));
+        //Verificando se não há nenhum erro na alocação
+        if (conjugada.mtx[i] == NULL) {
+            printf("Falha na alocação da memória\n");
+            //Liberando a memória em caso de erro
+            for (int j = 0; j < i; j++) {
+                free(conjugada.mtx[j]);
+            }
+            free(conjugada.mtx);
+            exit(1);
+        }
+    }
+
+    //Aqui vai a operação em si, que transforma a matriz original na sua conjjugada
+    for (int i = 0; i < matrix.linhas; i++) {
+        for(int j = 0; j < matrix.colunas; j++) {
+            conjugada.mtx[i][j].Re = matrix.mtx[i][j].Re;
+            conjugada.mtx[i][j].Im = -matrix.mtx[i][j].Im;//Inverte o sinal da parte imaginária
+        }
+    }
+
+    return conjugada;
+}
+
+void printComplex(complex num) {
+    printf("%.2f + %.2fi\n", num.Re, num.Im);
+}
+
 void printTransposta(complexMatrix transposta) {
     for (int l = 0; l < transposta.linhas; l++) {
         for (int c = 0; c < transposta.colunas; c++) {
             printf("transposta[%d][%d]: ", l, c);
             printComplex(transposta.mtx[l][c]);
+        }
+    }
+}
+
+void printConjugada(complexMatrix conjugada) {
+    printf("Matriz Conjugada: \n");
+    for (int l = 0; l < conjugada.linhas; l++) {
+        for (int c = 0; c < conjugada.colunas; c++) {
+            printf("conjugada[%d][%d]: ", l, c);
+            printComplex(conjugada.mtx[l][c]);
         }
     }
 }
@@ -110,6 +162,11 @@ int main() {
 
     printf("\nMatriz Transposta: \n");
     printTransposta(transposta);
+
+    complexMatrix conjugada = matrixConjugada(matrix);
+    
+    printf("\nMatrix Conjugada: \n");
+    printConjugada(conjugada);
 
     // Liberando a memória alocada para as matrizes complexas
     for (int i = 0; i < matrix.linhas; i++) {
