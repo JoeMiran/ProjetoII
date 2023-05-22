ff8aa27046b4b523cef733c3ebd5f281f028e5e8
davidpsousa <davidpinheirosousa.ds@gmail.com>
2023-05-22 10:53:32 -0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/main.c
    	modified:   src/matrizes.h

--- src/main.c
+++ src/main.c
@@ -1,16 +1,17 @@
+///@file main.c
 #include <stdio.h>
 #include <stdlib.h>
 
-// incluindo o arquivos onde está contido a estrutura
+/// incluindo o arquivos onde está contido a estrutura
 #include "matrizes.h"
 
-// Criando p protótipo da função que será chamada no escolpo da main e foi definida em matrizes.c
+/// Criando p protótipo da função que será chamada no escolpo da main e foi definida em matrizes.c
 void teste_todos();
 
 int main()
 {
 
-    // Chamando a função que irá imprimir todas as funções de teste
+    /// Chamando a função que irá imprimir todas as funções de teste
     teste_todos();
 
     return 0;
--- src/matrizes.h
+++ src/matrizes.h
@@ -2,24 +2,24 @@
 #define MATRIZES_H
 #include <stdio.h>
 
-// Definição da estrutura complexa
+/// Definição da estrutura complexa
 typedef struct
 {
     float Re, Im; // Campos para armazenar a parte real(Re) e imaginária (Im)
 } complex;
 
-// Definição da estrutura complexMatrix
+/// Definição da estrutura complexMatrix
 typedef struct
 {
     int linhas, colunas; // Campos para armazenar o número de linhas e de colunas
     complex **mtx;       // Definição de um ponteiro de ponteiros 'mtx' para uma matriz de números complexos
 } complexMatrix;
 
-/****************************************** DECLARAÇÃO DAS FUNÇÕES COMPLEXAS ****************************************************/
-/*
------> As funções abaixo estão sendo implementadas em 'matrizes.c'.
------> Abaixo temos apenas as assinaturas das respectivas funções.
-*/
+///****************************************** DECLARAÇÃO DAS FUNÇÕES COMPLEXAS ****************************************************/
+///
+///-----> As funções abaixo estão sendo implementadas em 'matrizes.c'.
+///-----> Abaixo temos apenas as assinaturas das respectivas funções.
+///
 complexMatrix allocateComplexMaatrix(int linhas, int colunas);
 complexMatrix matrixTransposta(complexMatrix matrix);
 complexMatrix matrixConjugada(complexMatrix matrix);
