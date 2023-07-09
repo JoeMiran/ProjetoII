dc2065bcd948f005031f246c118a85461424c7d6
JoeMiran <joemiran06@gmail.com>
2023-07-09 17:59:49 -0300

    Estou cansado, chefe :(  Your branch is up to date with 'origin/Funciona'.
     Mudanças a serem submetidas:

--- src/pds_telecom.c
+++ src/pds_telecom.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pds_telecom.h"
-
-typedef struct {
-    float real;
-    float img;
-} complexo;
-
+#include "matrizes.h"
 int * tx_data_read(FILE *file, long int sequencia_bytes) {
     
     // Aloca memória para o array de inteiros
@@ -50,10 +45,10 @@ void rx_data_write(int *s, long int sequencia_bytes, char *filename) {
     fclose(out);
 }
 
-complexo *tx_qam_mapper(int *s, long int qam) {
+complex *tx_qam_mapper(int *s, long int qam) {
     
-    // Aloca memória para o vetor de complexos
-    complexo *simbolo = (complexo *)malloc(qam * sizeof(complexo));
+    // Aloca memória para o vetor de complexs
+    complex *simbolo = (complex *)malloc(qam * sizeof(complex));
     
     if (simbolo == NULL) {
         printf("Erro na alocação de memória\n");
@@ -64,29 +59,54 @@ complexo *tx_qam_mapper(int *s, long int qam) {
     
         switch (s[i]) {
             case 0:
-                simbolo[i].real = -1;
-                simbolo[i].img = 1;
+                simbolo[i].Re = -1;
+                simbolo[i].Im = 1;
                 break;
             case 1:
-                simbolo[i].real = -1;
-                simbolo[i].img = -1;
+                simbolo[i].Re = -1;
+                simbolo[i].Im = -1;
                 break;
             case 2:
-                simbolo[i].real = 1;
-                simbolo[i].img = 1;
+                simbolo[i].Re = 1;
+                simbolo[i].Im = 1;
                 break;
             case 3:
-                simbolo[i].real = 1;
-                simbolo[i].img = -1;
+                simbolo[i].Re = 1;
+                simbolo[i].Im = -1;
                 break;
             default:
-                simbolo[i].real = 0;
-                simbolo[i].img = 0;
+                simbolo[i].Re = 0;
+                simbolo[i].Im = 0;
             break;
         }
     }
     return simbolo;
 }
+
+complex *tx_layer_mapper(complex *qam_simbolos, int num_simbolos, int num_streams) {
+    
+    // Calculate the number of symbols per stream
+    int simbolos_por_stream = num_simbolos / num_streams;
+
+    // Allocate memory for the mapped symbols
+    complex *mapped_simbolos = (complex *)malloc(num_simbolos * sizeof(complex));
+    if (mapped_simbolos == NULL) {
+        printf("Error allocating memory\n");
+        return NULL;
+    }
+
+    // Perform layer mapping
+    for (int i = 0; i < num_simbolos; i++) {
+        int stream_idx = i % num_streams;
+        int symbol_idx = i / num_streams;
+
+        // Map the QAM symbol to the corresponding stream
+        mapped_simbolos[i] = qam_simbolos[symbol_idx * num_streams + stream_idx];
+    }
+
+    return mapped_simbolos;
+}
+
 
 int main() {
 
@@ -106,7 +126,7 @@ int main() {
     long int file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
     
-    // Realiza a leitura do arquivo e obtém a sequência de dígitos
+    // Reiza a leitura do arquivo e obtém a sequência de dígitos
     int *resultado = tx_data_read(file, file_size);
 
     if (resultado != NULL) {
@@ -120,17 +140,30 @@ int main() {
 
         // Escreve a sequência de dígitos no arquivo binário
         rx_data_write(resultado, file_size, filename_saida);
+
         
-        // Mapeia os indices para os numeros complexos da constelacao qam
-        complexo *map = tx_qam_mapper(resultado, file_size * 4);
+        // Mapeia os indices para os numeros complexs da constelacao qam
+        complex *map = tx_qam_mapper(resultado, file_size * 4);
         
         if (map != NULL) {
             
-            // Imprime os numeros complexos
+            // Imprime os numeros complexs
             for (int i = 0; i < file_size * 4; i++) {
-                printf("Índice %d: %.2f%+.2fj\n", i, map[i].real, map[i].img);
+                printf("Índice %d: %.2f%+.2fj\n", i, map[i].Re, map[i].Im);
             }
-        
+
+            printf("\n");
+
+            // Mapeia os símbolos QAM para cada stream
+            int num_streams = 4; 
+            complex *mapped_symbols = tx_layer_mapper(map, file_size * 4, num_streams);
+
+            if (mapped_symbols != NULL) {
+                // Imprime os símbolos mapeados para cada stream
+                for (int i = 0; i < file_size * 4; i++) {
+                    printf("Stream %d: %.2f%+.2fj\n", i % num_streams, mapped_symbols[i].Re, mapped_symbols[i].Im);
+                }
+            }
         }
     }
 
--- src/pds_telecom.h
+++ src/pds_telecom.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <gsl/gsl_linalg.h>
+#include "matrizes.h"
 
 int * tx_data_read(FILE *file, long int sequencia_bytes);
 void rx_data_write(int *s, long int sequencia_bytes, char *filename);
-complexo *tx_qam_mapper(int *s, long int qam);
+complex *tx_qam_mapper(int *s, long int qam);
+complex *tx_layer_mapper(complex *qam_simbolos, int num_simbolos, int num_streams);
